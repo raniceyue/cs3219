262cc5fcdefb88599fa0eb9213e8367645a5ee5c
raniceyue <ranicetan68@gmail.com>
2020-08-22 14:35:07 +0800

    Add files

---
+++ a/application/index.js
@@ -0,0 +1,17 @@
+
+var http = require('http');
+var fs = require('fs');
+
+const PORT=3000;
+
+fs.readFile('./index.html', function(err, html) {
+
+	if (err) throw err;
+	
+	http.createServer(function (req, res) {
+ 		res.writeHead(200, {'Content-Type': 'text/html'});
+  		res.write(html);
+		res.end();
+	}).listen(PORT);
+
+});
